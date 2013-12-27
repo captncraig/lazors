e0e50e9d13b8013846745785d0bc838a2139d9c0
Craig Peterson <peterson.craig@gmail.com>
2013-12-26 23:22:11 -0700

    Formatting

--- lazors.go
+++ lazors.go
@@ -15,13 +15,13 @@ import (
 //011 - Shield - Impervious from one direction
 //100 - Mirror - Bounces according to direction:	Destroyed from back. 
 //					00: X| 		01:	XXX		10:	XXX		11:	  X
-//						X└-			X			  X			  X
-//						XXX			X			  X			XXX		(Bounce directions are OO and OO+1)
+//					    X└-			X			  X			  X
+//					    XXX			X			  X			XXX		(Bounce directions are OO and OO+1)
 //              Left hand Rule
 //101 - DoubleMirror - Bounces according to direction:
 //					00/10:	X			01/11:	  X
-//							 X					 X
-//							  X					X
+//						 X				 X
+//						  X			        X
 
 //Constants to define Cells. eg. Mirror | Silver | South
 const(
